2861c72267a217c47985cb39455db3321ee223cb
Polshchikov Yury <Polshchikov.Y.Va@sberbank.ru>
2019-10-29 22:21:40 +0300

    279

--- src/medium/package-info.java
+++ src/medium/package-info.java
@@ -1,5 +1,6 @@
 /**
- * 54.  Spiral Matrix                   https://leetcode.com/problems/spiral-matrix/
- * 831. Masking Personal Information    https://leetcode.com/problems/masking-personal-information/
+ * 54   Spiral Matrix                   https://leetcode.com/problems/spiral-matrix/
+ * 279  Perfect Squares                 https://leetcode.com/problems/perfect-squares/
+ * 831  Masking Personal Information    https://leetcode.com/problems/masking-personal-information/
  **/
 package medium;
---
+++ test/medium/T279PerfectSquaresTest.kt
@@ -0,0 +1,42 @@
+package medium
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class T279PerfectSquaresTest {
+
+    @Test
+    fun numSquares() {
+        val testClass = T279PerfectSquares()
+        assertEquals(1, testClass.numSquares(1))
+        assertEquals(2, testClass.numSquares(2))
+        assertEquals(3, testClass.numSquares(3))
+        assertEquals(2, testClass.numSquares(8))
+        assertEquals(1, testClass.numSquares(9))
+        assertEquals(3, testClass.numSquares(12))
+        assertEquals(2, testClass.numSquares(13))
+        assertEquals(3, testClass.numSquares(19))
+        assertEquals(3, testClass.numSquares(99))
+        assertEquals(3, testClass.numSquares(253))
+        assertEquals(2, testClass.numSquares(456853))
+    }
+
+    @Test
+    fun numSquaresShortSolution() {
+        val testClass = T279PerfectSquaresShortSolution()
+        assertEquals(2, testClass.numSquares(13))
+        assertEquals(3, testClass.numSquares(99))
+        assertEquals(2, testClass.numSquares(456853))
+    }
+
+    @Test
+    fun numSquaresBFSSolution() {
+        val testClass = T279PerfectSquaresBFSSolution()
+        assertEquals(2, testClass.numSquares(13))
+        assertEquals(1, testClass.numSquares(16))
+        assertEquals(3, testClass.numSquares(19))
+        assertEquals(2, testClass.numSquares(456853))
+
+    }
+}
