aca7fe3a7ce724b6e308fa44102a2f0496e7d2a3
Polshchikov Yury <Polshchikov.Y.Va@sberbank.ru>
2019-10-29 23:44:53 +0300

    1160

--- src/easy/package-info.java
+++ src/easy/package-info.java
@@ -4,5 +4,6 @@
  * 38   Count and Say           https://leetcode.com/problems/count-and-say/
  * 278  First Bad Version       https://leetcode.com/problems/first-bad-version/
  * 349  IntersectionOfTwoArrays https://leetcode.com/problems/intersection-of-two-arrays/
+ * 1160 Find Words That Can Be Formed by Characters https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
  */
 package easy;
