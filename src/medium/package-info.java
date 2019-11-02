a6c512e9781b6614ac8bbc744e391b6183e4045f
Polshchikov Yury <Polshchikov.Y.Va@sberbank.ru>
2019-11-02 19:35:29 +0300

    535

--- src/medium/package-info.java
+++ src/medium/package-info.java
@@ -1,6 +1,7 @@
 /**
  * 54   Spiral Matrix                   https://leetcode.com/problems/spiral-matrix/
  * 279  Perfect Squares                 https://leetcode.com/problems/perfect-squares/
+ * 535  Encode and Decode TinyURL       https://leetcode.com/problems/encode-and-decode-tinyurl/
  * 831  Masking Personal Information    https://leetcode.com/problems/masking-personal-information/
  **/
 package medium;
