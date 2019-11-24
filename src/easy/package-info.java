95417a15eb0066994e756cf13a15395310cf43a5
Polshchikov Yury <Polshchikov.Y.Va@sberbank.ru>
2019-11-24 10:42:37 +0300

    9

--- src/easy/package-info.java
+++ src/easy/package-info.java
@@ -1,4 +1,5 @@
 /**
+ * 9    Palindrome Number       https://leetcode.com/problems/palindrome-number/
  * 13   Roman to Integer        https://leetcode.com/problems/roman-to-integer/
  * 35   Search Insert Position  https://leetcode.com/problems/search-insert-position/
  * 38   Count and Say           https://leetcode.com/problems/count-and-say/
